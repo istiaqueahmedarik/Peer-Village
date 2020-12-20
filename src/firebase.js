d1bfdc95a959f03c81f093ce1353417463204c77
istiaqueahmedarik <69030256+istiaqueahmedarik@users.noreply.github.com>
2020-12-20 14:58:59 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC7r2MbTkAYQ42ADYUXg_2PiQZ4Nw0g_8A",
-    authDomain: "peer-village.firebaseapp.com",
-    databaseURL: "https://peer-village.firebaseio.com",
-    projectId: "peer-village",
-    storageBucket: "peer-village.appspot.com",
-    messagingSenderId: "386469041118",
-    appId: "1:386469041118:web:cf10793c8134be9353c34f",
-    measurementId: "G-H7VNGJ5242" 
+    //your config
 });
 
 const db = firebaseApp.firestore();
